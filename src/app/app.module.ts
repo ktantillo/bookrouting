e155f1fb7e4a342c76bf6d60a4a3c54992f3fa9b
ktantillo <96933702+ktantillo@users.noreply.github.com>
2023-02-17 15:31:10 -0500

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AboutComponent } from './about/about.component';
+import { BookComponent } from './book/book.component';
+import { BlogComponent } from './blog/blog.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AboutComponent,
+    BookComponent,
+    BlogComponent,
+    HomeComponent,
+    ContactComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
